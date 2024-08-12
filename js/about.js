704cf21e25c58d2e9505eccc613127c5374727f3
AzimObidov <azimazimazim2009@gmail.com>
2024-08-12 07:50:02 +0500

    yangi

---
+++ js/about.js
@@ -0,0 +1,93 @@
+// let a = prompt("a qiymatini kiriting");
+// let b = prompt("b qiymatini kiriting");
+// let d = prompt("d qiymatini kiriting");
+// let c = prompt("c qiymatini kiriting");
+// let r = prompt("r qiymatini kiriting");
+// let x = prompt('x qiymatini kiriting');
+// let y = prompt('y qiymatini kiriting')
+// let alfa = 3.14;
+// 3
+// const s = a * b;
+// const p = 2*(+a + +b);
+// console.log('yuza:',s,'    ','peremetr:',p);
+
+// 4
+// const l = alfa * d;
+// console.log('uzunligi:',l);
+
+// 5
+// const v = a * a * a;
+// const s = 6 * a * a;
+// console.log('hajm:',v,'    ','sirti:',s);
+
+// 6
+// const v = a * b * c;
+// const s = 2 * (a * +b + +b * +c + +a * c);
+// console.log('hajm:',v,'    ','sirti:',s);
+
+// 7
+// const l = 2 * alfa * r;
+// const s = 2 * (r * r);
+// console.log('uzunligi',l,'    ','yuzasi',s);
+
+// 8
+// const o = (+a + +b) / 2;
+// console.log(o);
+
+// 9
+// const v = +a + +b;
+// const sum = Math.sqrt(v);
+// console.log(sum);
+
+// 10
+// const q = +a + +b;
+// const w = a * b;
+// const e = (+a + +b) * (+a + +b)
+// console.log('yigindisi=',q,'     ','kopaytmasi=',w,'     ','kvadrati=',e)
+
+// 11
+// const q = +a + +b;
+// const w = a * b;
+// const e = a % b;
+// console.log('yigindisi=',q,'     ','kopaytmasi=',w,'     ','moduli=',e)
+
+// 12
+// const q = ( ( a * a ) + ( b * b ) )
+// const w = Math.sqrt(q)
+// const e = +a + +b + +c
+// console.log('gipotenuzasi=',w,'    ','peremetri=',e);
+
+// 14
+// const q = 2 * alfa * r
+// const w = alfa + (r * r)
+// console.log('uzunligi=',q,'      ','yuzasi=',w)
+
+// 15
+// const w = alfa + (r * r)
+// console.log('diametri=',d,'     ','radiusi',r,'      ','yuzasi=',w)
+
+// 26
+// const z = 4 * ( ( +x - +3 ) ** 6) - +7 * ( ( +x - +3) ** 3) + +2
+// console.log('yigindi',z)
+
+// 27
+// const q = a ** 2
+// const w = a ** 4
+// const e = a ** 8
+// console.log(q,'   ',w,'   ',e)
+
+// 28
+// const q = a ** 2
+// const w = a ** 3
+// const e = a ** 5
+// const t = a ** 10
+// const z = a ** 15
+// console.log(q,'   ',w,'   ',e,'   ',t,'   ',z)
+
+// 33
+// const q = 1 * a
+// const e = y * a
+// console.log('1kg konfet narxi=',q,'    ','y konfet narxi=',e)
+
+
+
